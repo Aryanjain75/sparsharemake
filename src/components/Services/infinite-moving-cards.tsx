a6a5b2908a079540ed2ccf5cd98412a06bae6cf7
ARYAN1JAIN <jainaryanjain30@gmail.com>
2024-07-28 04:58:59 +0530

    Added new feature and fixed bugs

--- src/components/Services/infinite-moving-cards.tsx
+++ src/components/Services/infinite-moving-cards.tsx
@@ -128,7 +128,7 @@ export const InfiniteMovingCards = ({
                   translateZ="60"
                   className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
-                  "{item.quot}"
+                  &quot{item.quot}&quot
                 </CardItem>
               </CardBody>
             </CardContainer>
