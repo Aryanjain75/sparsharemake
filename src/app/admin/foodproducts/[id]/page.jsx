d5be3af22217c57906029c5a9e8788120bf0a0f6
ARYAN1JAIN <jainaryanjain30@gmail.com>
2024-07-28 03:45:34 +0530

    Added new feature and fixed bugs

--- src/app/admin/foodproducts/[id]/page.jsx
+++ src/app/admin/foodproducts/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Update from "@/components/products/update";
+import Update from "@/components/foodproducts/update";
 export default function MenuPage ({ params })  {
   const [menuData, setMenuData] = useState(null);
   const [loading, setLoading] = useState(true);
